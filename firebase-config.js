42b2822a5b9df3e11bb8574542ae49bd440c7cbb
MD SHARIFUL ISLAM <94898754+msharifulvisionary@users.noreply.github.com>
2025-09-07 20:01:36 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfzU7BDZGyRA8_7sdYkDe0C-ZcvDAtGuU",
+  authDomain: "college-dashboard-ahc.firebaseapp.com",
+  projectId: "college-dashboard-ahc",
+  storageBucket: "college-dashboard-ahc.firebasestorage.app",
+  messagingSenderId: "692197968743",
+  appId: "1:692197968743:web:ed9d77e7efed11ae1efdc2",
+}
+
+export default firebaseConfig
